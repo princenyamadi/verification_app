6febb71bce3ac00168a6ebd739c2504261cc522c
princenyamadi <princenyamadi.pn@gmail.com>
2022-02-12 10:26:41 +0000

    camera package integration

---
+++ lib/screens/camera_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({Key? key}) : super(key: key);
+
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
